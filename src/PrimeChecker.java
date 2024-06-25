df2216cfba65723f3c91d086942b2fd454fbef10
Steveb-cloud <esteban.henriquez@tecnologicoliceocristiano.edu.ec>
2024-06-25 16:00:26 -0400

    Initial commit

---
+++ src/PrimeChecker.java
@@ -0,0 +1,23 @@
+public class PrimeChecker {
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 29;
+        if (isPrime(number)) {
+            System.out.println(number + " es un número primo.");
+        } else {
+            System.out.println(number + " no es un número primo.");
+        }
+    }
+}
